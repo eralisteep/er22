5c5c1b353a8d0e89ed01c3f026001b4e7e293d05
eralisteep <eralirahat@gmail.com>
2025-09-26 10:02:00 +0500

    new's title became link to the new's page

--- src/app/novosti-kolledzha/page.tsx
+++ src/app/novosti-kolledzha/page.tsx
@@ -21,39 +21,40 @@ export default function Page() {
         <p>Загрузка...</p>
       ) : (
         news.map((item, index) => (
-          <div key={index} style={{ marginBottom: 30 }}>
-            <h2 style={{ maxWidth: '100%'}}>{item.title}</h2>
-            <div dangerouslySetInnerHTML={{ __html: item.description }} />
-
-            {/* Проверка типа файла */}
-            {item.file && item.file.match(/\.(jpeg|jpg|png|gif|webp)$/i) ? (
-              <img
-                src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/news-files/${item.file}`}
-                alt="preview"
-                style={{ maxWidth: '100%', borderRadius: 8 }}
-              />
-            ) : item.file && item.file.match(/\.(mp4|webm|ogg)$/i) ? (
-              <video
-                controls
-                style={{ maxWidth: '100%', borderRadius: 8 }}
-              >
-                <source
-                  src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/news-files/${item.file}`}
-                  type={`video/${item.file.split('.').pop()}`}
-                />
-                Ваш браузер не поддерживает воспроизведение видео.
-              </video>
-            ) : item.file ? (
-              <a
-                href={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/news-files/${item.file}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                📎 Открыть файл
+            <div key={index} style={{ marginBottom: 30 }}>
+              <a href={"/novosti-kolledzha/" + item.id}>
+                <h2 style={{ maxWidth: '100%'}}>{item.title}</h2>
               </a>
-            ) : null}
-            <p><i>{new Date(item.created_at).toLocaleString()}</i></p>
-          </div>
+              <div dangerouslySetInnerHTML={{ __html: item.description }} />
+              {/* Проверка типа файла */}
+              {item.file && item.file.match(/\.(jpeg|jpg|png|gif|webp)$/i) ? (
+                <img
+                  src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/news-files/${item.file}`}
+                  alt="preview"
+                  style={{ maxWidth: '100%', borderRadius: 8 }}
+                />
+              ) : item.file && item.file.match(/\.(mp4|webm|ogg)$/i) ? (
+                <video
+                  controls
+                  style={{ maxWidth: '100%', borderRadius: 8 }}
+                >
+                  <source
+                    src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/news-files/${item.file}`}
+                    type={`video/${item.file.split('.').pop()}`}
+                  />
+                  Ваш браузер не поддерживает воспроизведение видео.
+                </video>
+              ) : item.file ? (
+                <a
+                  href={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/news-files/${item.file}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  📎 Открыть файл
+                </a>
+              ) : null}
+              <p><i>{new Date(item.created_at).toLocaleString()}</i></p>
+            </div>
         ))
       )}
     </div>
